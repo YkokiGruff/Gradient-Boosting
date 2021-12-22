127629dd55d52fc353a6d25ac2e09f22006a9dcc
YkokiGruff202112 <96454652+YkokiGruff202112@users.noreply.github.com>
2021-12-22 12:37:34 +0900

    Update LigjtGBM.py

--- LigjtGBM.py
+++ LigjtGBM.py
@@ -4,20 +4,16 @@ import numpy as np
 from tensorflow.keras.datasets import mnist
 from sklearn.model_selection import train_test_split
 
-
-# Kerasに付属の手書き数字画像データをダウンロード
+# 以下Catboostのコードを参照してください
 np.random.seed(0)
 (X_train_base, labels_train_base), (X_test, labels_test) = mnist.load_data()
 
-# Training set を学習データ（X_train, labels_train）と検証データ（X_validation, labels_validation）に8:2で分割する
 X_train,X_validation,labels_train,labels_validation = train_test_split(X_train_base,labels_train_base,test_size = 0.2)
 
-# 各画像は行列なので1次元に変換→X_train,X_validation,X_testを上書き
 X_train = X_train.reshape(-1,784)
 X_validation = X_validation.reshape(-1,784)
 X_test = X_test.reshape(-1,784)
 
-#正規化
 X_train = X_train.astype('float32')
 X_validation = X_validation.astype('float32')
 X_test = X_test.astype('float32')
@@ -25,13 +21,12 @@ X_train /= 255
 X_validation /= 255
 X_test /= 255
 
-# 訓練・検証データの設定
 train_data = lgb.Dataset(X_train, label=labels_train)
 eval_data = lgb.Dataset(X_validation, label=labels_validation, reference= train_data)
 
-#経過時間計測
 import time
 start = time.time()
+
 
 #light gbm モデル構築
 params = {
